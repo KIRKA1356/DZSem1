fbdd95928b0f681eb7528f01f2e903288966625a
KIRKA1356 <kgritsun@mail.ru>
2022-06-19 22:55:25 +0300

    4я задача готова!

--- Program.cs
+++ Program.cs
@@ -1,13 +1,15 @@
 ﻿Console.WriteLine("Введите число: ");
 int n1 = Convert.ToInt32(Console.ReadLine());
 
-int n2 = n1 % 2;
+int n2 = 2;
+int count=0;
 
-if (n2 >0)
+while (count < n1)
 {
-    Console.WriteLine("Число нечетное");
+    if (n2<=n1)
+    {
+    Console.Write(" " + n2);
+    }
+    n2=n2+2;
+    count= count +2;
 }
-    else
-        {
-            Console.WriteLine("Число четное");
-        }
