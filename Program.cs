ad1b344dc134aa30eaef20ca4ec20c354c16d435
KIRKA1356 <kgritsun@mail.ru>
2022-06-19 22:31:26 +0300

    1я задача готова!

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("ВВедите первое число: ");
+int n1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("ВВедите второе число: ");
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+if (n1>n2)
+{
+    Console.WriteLine("Большее число " + n1);
+    Console.WriteLine("Меньшее число " + n2);
+}
+    else
+        if (n1<n2)
+        {
+            Console.WriteLine("Большее число " + n2);
+            Console.WriteLine("Меньшее число " + n1);
+        }
+            else
+            {
+                Console.WriteLine("Числа равны");
+            }
